aa3bbb93d69e3c3510a33a461bc4b7edf108e7a5
tech_rounak <tech.rounak12@gmail.com>
2022-06-22 12:11:42 +0530

    changes in databse

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/EmployeeDB',{
+mongoose.connect('mongodb+srv://tech_rounak:rounak_atlas1@cluster0.hcmth.mongodb.net/?retryWrites=true&w=majority',{
      useNewUrlParser:true,
      useUnifiedTopology:true
 }).then(()=>{
